3b17e87fbb07a94cbbba3b12c74dc164412a8bcc
Siegerus <ivanoffsiegerus@gmail.com>
2026-04-18 19:48:36 +0300

    fix/fixed-vite-scss-paths

--- src/hooks/useDraggble.ts
+++ src/hooks/useDraggble.ts
@@ -1,131 +1,131 @@
-import { useEffect, useState, MutableRefObject } from 'react';
+// import { useEffect, useState, MutableRefObject } from 'react';
 
-export enum DraggingState {
-	undefined = -1,
-	starts = 0,
-	moves = 1,
-	finished = 2
-}
+// export enum DraggingState {
+// 	undefined = -1,
+// 	starts = 0,
+// 	moves = 1,
+// 	finished = 2
+// }
 
-export function useDragging(
-	ref: MutableRefObject<HTMLDivElement | null>,
-	elementHeight: number
-) {
-	// const ref = useRef() as RefObject<HTMLButtonElement>;
-	const [state, setState] = useState(DraggingState.undefined);
-	const [point, setPoint] = useState({ x: 0, y: 0 }); // point of cursor in relation to the element's parent точка курсора относительно родительского элемента
-	const [elementOffset, setElementOffset] = useState({
-		x: 0,
-		y: ref.current?.offsetTop
-	}); // offset of element in relation to it's parent смещение элемента по отношению к его родительскому элементу
-	const [touchOffset, setTouchOffset] = useState({ x: 0, y: 0 }); // offset of mouse down point in relation to the element
+// export function useDragging(
+// 	ref: MutableRefObject<HTMLDivElement | null>,
+// 	elementHeight: number
+// ) {
+// 	// const ref = useRef() as RefObject<HTMLButtonElement>;
+// 	const [state, setState] = useState(DraggingState.undefined);
+// 	const [point, setPoint] = useState({ x: 0, y: 0 }); // point of cursor in relation to the element's parent точка курсора относительно родительского элемента
+// 	const [elementOffset, setElementOffset] = useState({
+// 		x: 0,
+// 		y: ref.current?.offsetTop
+// 	}); // offset of element in relation to it's parent смещение элемента по отношению к его родительскому элементу
+// 	const [touchOffset, setTouchOffset] = useState({ x: 0, y: 0 }); // offset of mouse down point in relation to the element
 
-	// shows active state of dragging
-	const isDragging = () => {
-		return state === DraggingState.starts || state === DraggingState.moves;
-	};
+// 	// shows active state of dragging
+// 	const isDragging = () => {
+// 		return state === DraggingState.starts || state === DraggingState.moves;
+// 	};
 
-	function onMouseDown(e: MouseEvent) {
-		// e.preventDefault();
-		// console.log('down');
-		const parentElement = ref.current?.offsetParent as HTMLElement;
-		if (/* e.button !== 0 ||  */ !ref.current || !parentElement) return;
+// 	function onMouseDown(e: MouseEvent) {
+// 		// e.preventDefault();
+// 		// console.log('down');
+// 		const parentElement = ref.current?.offsetParent as HTMLElement;
+// 		if (/* e.button !== 0 ||  */ !ref.current || !parentElement) return;
 
-		// First entry to the flow.
-		// We save touchOffset value as parentElement's offset
-		// to calculate element's offset on the move.
-		setPoint({
-			x: e.x - parentElement.offsetLeft,
-			y: e.y - parentElement.offsetTop
-		});
-		setElementOffset({
-			x: ref.current.offsetLeft,
-			y: ref.current.offsetTop
-		});
-		setTouchOffset({
-			x: e.x - parentElement.offsetLeft - ref.current.offsetLeft,
-			y: e.y - parentElement.offsetTop - ref.current.offsetTop
-		});
+// 		// First entry to the flow.
+// 		// We save touchOffset value as parentElement's offset
+// 		// to calculate element's offset on the move.
+// 		setPoint({
+// 			x: e.x - parentElement.offsetLeft,
+// 			y: e.y - parentElement.offsetTop
+// 		});
+// 		setElementOffset({
+// 			x: ref.current.offsetLeft,
+// 			y: ref.current.offsetTop
+// 		});
+// 		setTouchOffset({
+// 			x: e.x - parentElement.offsetLeft - ref.current.offsetLeft,
+// 			y: e.y - parentElement.offsetTop - ref.current.offsetTop
+// 		});
 
-		setState(DraggingState.starts);
-	}
+// 		setState(DraggingState.starts);
+// 	}
 
-	function onMouseMove(e: MouseEvent) {
-		e.preventDefault();
-		// console.log('move');
-		const parentElement = ref.current?.offsetParent as HTMLElement;
-		if (!isDragging() || !ref.current || !parentElement) return;
-		setState(DraggingState.moves);
+// 	function onMouseMove(e: MouseEvent) {
+// 		e.preventDefault();
+// 		// console.log('move');
+// 		const parentElement = ref.current?.offsetParent as HTMLElement;
+// 		if (!isDragging() || !ref.current || !parentElement) return;
+// 		setState(DraggingState.moves);
 
-		setPoint({
-			x: e.x - parentElement.offsetLeft,
-			y: e.y - parentElement.offsetTop
-		});
+// 		setPoint({
+// 			x: e.x - parentElement.offsetLeft,
+// 			y: e.y - parentElement.offsetTop
+// 		});
 
-		setElementOffset({
-			x: e.x - touchOffset.x - parentElement.offsetLeft,
-			y: e.y - touchOffset.y - parentElement.offsetTop
-		});
+// 		setElementOffset({
+// 			x: e.x - touchOffset.x - parentElement.offsetLeft,
+// 			y: e.y - touchOffset.y - parentElement.offsetTop
+// 		});
 
-		console.log(elementHeight);
-	}
+// 		console.log(elementHeight);
+// 	}
 
-	function onMouseUp(e: MouseEvent) {
-		// console.log('up');
-		// ends up the flow by setting the state
-		setState(DraggingState.finished);
-	}
+// 	function onMouseUp(e: MouseEvent) {
+// 		// console.log('up');
+// 		// ends up the flow by setting the state
+// 		setState(DraggingState.finished);
+// 	}
 
-	function onClick(e: MouseEvent) {
-		// that's a fix for touch pads that transfer touches to click,
-		// e.g "Tap to click" on macos. When enabled, on tap mouseDown is fired,
-		// but mouseUp isn't. In this case we invoke mouseUp manually, to trigger
-		// finishing state;
-		setState(DraggingState.finished);
-	}
+// 	function onClick(e: MouseEvent) {
+// 		// that's a fix for touch pads that transfer touches to click,
+// 		// e.g "Tap to click" on macos. When enabled, on tap mouseDown is fired,
+// 		// but mouseUp isn't. In this case we invoke mouseUp manually, to trigger
+// 		// finishing state;
+// 		setState(DraggingState.finished);
+// 	}
 
-	// When the element mounts, attach an mousedown listener
-	useEffect(() => {
-		const element = ref.current;
-		element?.addEventListener('mousedown', onMouseDown);
-		element?.addEventListener('touchstart', onMouseDown);
+// 	// When the element mounts, attach an mousedown listener
+// 	useEffect(() => {
+// 		const element = ref.current;
+// 		element?.addEventListener('mousedown', onMouseDown);
+// 		element?.addEventListener('touchstart', onMouseDown);
 
-		return () => {
-			element?.removeEventListener('mousedown', onMouseDown);
-			element?.removeEventListener('touchstart', onMouseDown);
-		};
-	}, [ref.current]);
+// 		return () => {
+// 			element?.removeEventListener('mousedown', onMouseDown);
+// 			element?.removeEventListener('touchstart', onMouseDown);
+// 		};
+// 	}, [ref.current]);
 
-	// Everytime the state changes, assign or remove
-	// the corresponding mousemove, mouseup and click handlers
-	useEffect(() => {
-		if (isDragging()) {
-			document.addEventListener('mouseup', onMouseUp);
-			document.addEventListener('touchend', onMouseUp);
-			document.addEventListener('mousemove', onMouseMove);
-			document.addEventListener('touchmove', onMouseMove);
-			document.addEventListener('click', onClick);
-		} else {
-			document.removeEventListener('mouseup', onMouseUp);
-			document.removeEventListener('mousemove', onMouseMove);
-			document.removeEventListener('click', onClick);
-		}
-		return () => {
-			document.removeEventListener('mouseup', onMouseUp);
-			document.removeEventListener('touchend', onMouseUp);
-			document.removeEventListener('mousemove', onMouseMove);
-			document.removeEventListener('touchmove', onMouseMove);
-			document.removeEventListener('click', onClick);
-		};
-	}, [state]);
+// 	// Everytime the state changes, assign or remove
+// 	// the corresponding mousemove, mouseup and click handlers
+// 	useEffect(() => {
+// 		if (isDragging()) {
+// 			document.addEventListener('mouseup', onMouseUp);
+// 			document.addEventListener('touchend', onMouseUp);
+// 			document.addEventListener('mousemove', onMouseMove);
+// 			document.addEventListener('touchmove', onMouseMove);
+// 			document.addEventListener('click', onClick);
+// 		} else {
+// 			document.removeEventListener('mouseup', onMouseUp);
+// 			document.removeEventListener('mousemove', onMouseMove);
+// 			document.removeEventListener('click', onClick);
+// 		}
+// 		return () => {
+// 			document.removeEventListener('mouseup', onMouseUp);
+// 			document.removeEventListener('touchend', onMouseUp);
+// 			document.removeEventListener('mousemove', onMouseMove);
+// 			document.removeEventListener('touchmove', onMouseMove);
+// 			document.removeEventListener('click', onClick);
+// 		};
+// 	}, [state]);
 
-	return {
-		state: state,
-		point: point,
-		elementOffset: elementOffset,
-		touchOffset: touchOffset
-	};
-}
+// 	return {
+// 		state: state,
+// 		point: point,
+// 		elementOffset: elementOffset,
+// 		touchOffset: touchOffset
+// 	};
+// }
 
 // import {
 // 	useState,
