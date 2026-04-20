a3e1ae88b7421370919ca8f68d9906ba253585e1
Siegerus <ivanoffsiegerus@gmail.com>
2026-04-20 17:26:08 +0300

    up-to-date

--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -6,6 +6,7 @@ type PopupProps = PropsWithChildren<{
 	titleText: string;
 	titleClass: string;
 	wrappClass: string;
+	headlineClass: string;
 	onCloseClick: (idx: number | null) => void;
 }>;
 
@@ -14,15 +15,22 @@ const Popup = ({
 	titleText,
 	titleClass,
 	wrappClass,
+	headlineClass,
 	onCloseClick
 }: PopupProps) => {
 	const wrapperClass = `${wrappClass}`;
 	return (
 		<div className={wrapperClass}>
-			<div className={styles.headline}>
+			<div className={`${styles.headline} ${headlineClass}`}>
 				<Title titleText={titleText} titleClass={titleClass} />
-				<button type="button" onClick={() => onCloseClick(null)}>
-					Close
+				<button
+					className={styles.button}
+					type="button"
+					onClick={() => onCloseClick(null)}
+				>
+					<svg className={styles.button__icon} viewBox="0 0 18 18">
+						<use xlinkHref="#close"></use>
+					</svg>
 				</button>
 			</div>
 			{children}
--- src/pages/cake-article-page/cake-article-page.tsx
+++ src/pages/cake-article-page/cake-article-page.tsx
@@ -8,6 +8,7 @@ import Slider from './slider/slider';
 import OrderForm from './order-form/order-form';
 import styles from './cake-article-page.module.scss';
 import { useState } from 'react';
+import Button from '../../components/ui/button/button';
 
 const CakeArticlePage = () => {
 	const pageTitle = 'Торт с ягодами и безе';
@@ -23,10 +24,7 @@ const CakeArticlePage = () => {
 	const [visibilityIndex, setVisibilityIndex] = useState<number | null>(null);
 
 	const handleDescribeClick = (idx: number | null) => {
-		setVisibilityIndex(prevState => {
-			prevState = idx;
-			return prevState;
-		});
+		setVisibilityIndex(idx);
 	};
 
 	return (
@@ -65,7 +63,29 @@ const CakeArticlePage = () => {
 						titleClass={popupTitleClass}
 						titleText={fillingItem.title}
 						onCloseClick={handleDescribeClick}
-					></Popup>
+						headlineClass={styles.test}
+					>
+						<p className={styles.popup__description}>
+							{fillingItem.description}
+						</p>
+						<Button
+							className={`button button_primary ${styles.popup__button}`}
+							type="button"
+						>
+							<span>Понятно, спасибо</span>
+						</Button>
+						<ul className={styles.popup__list}>
+							<li>Вишня</li>
+							<li>
+								Йогуртовый крем на основе натурального йогурта
+							</li>
+							<li>Сливки</li>
+							<li>Бисквит ванильный</li>
+							<li>Вишневый сироп</li>
+							<li>Сливочное масло</li>
+							<li>Натуральный ванилин</li>
+						</ul>
+					</Popup>
 				);
 			})}
 		</div>
