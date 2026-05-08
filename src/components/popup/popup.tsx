716218ef584c653ba5244b6bad8d3a720fafe32d
Siegerus <ivanoffsiegerus@gmail.com>
2026-05-08 18:06:35 +0300

    chore/mobile-popup-final-animate

--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler, PropsWithChildren } from 'react';
+﻿import { MouseEventHandler, PropsWithChildren, AnimationEvent } from 'react';
 
 import styles from './popup.module.scss';
 
@@ -6,21 +6,22 @@ type PopupProps = PropsWithChildren<{
 	outerClass: string;
 	closeClass?: string;
 	onCloseClick: MouseEventHandler<HTMLButtonElement>;
+	onAnimationEnd?: (e: AnimationEvent) => void;
 }>;
 const Popup = ({
 	children,
 	outerClass,
 	closeClass,
-	onCloseClick
+	onCloseClick,
+	onAnimationEnd
 }: PopupProps) => {
 	return (
-		<div className={outerClass}>
+		<div className={outerClass} onAnimationEnd={onAnimationEnd}>
 			<button className={closeClass} type="button" onClick={onCloseClick}>
 				<svg className={styles.button__icon} viewBox="0 0 18 18">
 					<use xlinkHref="#close"></use>
 				</svg>
 			</button>
-
 			{children}
 		</div>
 	);
