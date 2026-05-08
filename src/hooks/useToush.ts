a7e6069ea6020499502dc1033f4e1de869807d0b
Siegerus <ivanoffsiegerus@gmail.com>
2026-05-08 19:16:06 +0300

    chore/add-custom-hook-for-touch-close

---
+++ src/hooks/useToush.ts
@@ -0,0 +1,43 @@
+import { useState, TouchEvent } from 'react';
+
+function useTouch(callback: () => void): {
+	handleTouchStart: (e: TouchEvent) => void;
+	handleTouchMove: (e: TouchEvent) => void;
+	handleTouchEnd: () => void;
+} {
+	const [isTouchMove, setIsTouchMove] = useState<boolean>(false);
+
+	const [touchStartY, setTouchStartY] = useState<number>(0);
+	const [touchEndY, setTouchEndY] = useState<number>(0);
+
+	const handleTouchStart = (e: TouchEvent) => {
+		setTouchStartY(e.touches[0].clientY);
+	};
+
+	const handleTouchMove = (e: TouchEvent) => {
+		setIsTouchMove(true);
+		setTouchEndY(e.touches[0].clientY);
+	};
+
+	const handleTouchEnd = () => {
+		if (!isTouchMove) return;
+
+		const touchDifference = touchEndY - touchStartY;
+		const touchLength = Math.abs(touchEndY - touchStartY);
+		const touchMinLength = 20;
+
+		if (touchLength < touchMinLength) return;
+
+		if (touchDifference > 0) callback();
+
+		setIsTouchMove(false);
+	};
+
+	return {
+		handleTouchStart,
+		handleTouchMove,
+		handleTouchEnd
+	};
+}
+
+export default useTouch;
