0be42cd7c5879b679f6d1a55df94f655187f4374
Siegerus <ivanoffsiegerus@gmail.com>
2026-05-09 06:51:19 +0300

    refactor/getFormattedPrice

--- src/utils/getFormattedPrice.ts
+++ src/utils/getFormattedPrice.ts
@@ -1,9 +1,5 @@
 const getFormattedPrice = (price: number): string => {
-	const str = price.toString();
-	console.log(str.length);
-	if (str.length === 5) return `${str[0]}${str[1]} ${str.slice(2)}`;
-	if (str.length > 5) return `${str[0]}${str[1]}${str[2]} ${str.slice(3)}`;
-	return `${str[0]} ${str.slice(1)}`;
+	return price.toLocaleString('ru-RU');
 };
 
 export default getFormattedPrice;
