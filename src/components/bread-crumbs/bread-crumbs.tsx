773290374d52982caa0187d4a73d7bbb27b69700
Siegerus <ivanoffsiegerus@gmail.com>
2026-05-13 20:12:31 +0300

    refactor/bread-crumbs-component

--- src/components/bread-crumbs/bread-crumbs.tsx
+++ src/components/bread-crumbs/bread-crumbs.tsx
@@ -1,22 +1,28 @@
 import { Link, useLocation } from 'react-router-dom';
 
 import { AppRoute, NAVS } from '../../constants';
-import { getNavData } from '../../utils/getNavData';
+import getNavData from '../../utils/getNavData';
 import styles from './bread-crumbs.module.scss';
 
 const BreadCrumbs = () => {
 	const { pathname } = useLocation();
 
-	const linkTitle = getNavData(pathname, NAVS)!.title;
-	const linkPath = getNavData(pathname, NAVS)!.path;
+	const navData = getNavData(pathname, NAVS);
+
+	if (!navData) {
+		// Если навигационные данные не найдены, не отображаем крошки
+		return null;
+	}
+
+	const { title, path } = navData;
 
 	return (
-		<nav className={styles.component}>
+		<nav className={styles.component} aria-label="Хлебные крошки">
 			<Link className={styles.link} to={AppRoute.Root}>
 				Главная
 			</Link>
-			<Link className={styles.link} to={linkPath}>
-				{linkTitle}
+			<Link className={styles.link} to={path} aria-current="page">
+				{title}
 			</Link>
 		</nav>
 	);
