bb269e6f1ed2bd75a595cc81d20dec1a5ff268e1
Siegerus <ivanoffsiegerus@gmail.com>
2026-05-20 07:53:29 +0300

    feat(about-page): add about page
    Introduce crimson color variables and IMPORTANCE constant.
    Implement full page layout including breadcrumbs, titles,
    description and styled highlights block. Adjust catalog hr
    color for visual consistency.

--- src/pages/about-page/about-page.tsx
+++ src/pages/about-page/about-page.tsx
@@ -1,15 +1,34 @@
 import BreadCrumbs from '../../components/bread-crumbs/bread-crumbs';
+import Title from '../../components/title/title';
+import { IMPORTANCE } from '../../constants';
 import styles from './about-page.module.scss';
 
 type Props = {};
 
 const AboutPage = (props: Props) => {
 	return (
-		<main className={`page ${styles.page}`}>
-			<h1 className="visually-hidden">О нас</h1>
-			<p>AboutPage</p>
-			<BreadCrumbs />
-		</main>
+		<div className="container">
+			<div className={`page ${styles.page}`}>
+				<BreadCrumbs />
+				<Title titleText="О нас" titleClass={`title ${styles.title}`} />
+				<hr />
+
+				<main className={styles.main}>
+					<Title
+						titleText="Вкусно, красиво и 
+						с любовью"
+						level="h2"
+						titleClass={`title title_fw800 ${styles.main__title}`}
+					/>
+
+					<p className={styles.main__description}>
+						ВауКейк — современная кондитерская в онлайн-формате. Мы
+						не просто готовим десерты, а помогаем выразить чувства и
+						сделать ваш праздник особенным.
+					</p>
+				</main>
+			</div>
+		</div>
 	);
 };
 
