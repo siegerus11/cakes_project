a7348af93658322e742d717d255054836e2e030f
Siegerus <ivanoffsiegerus@gmail.com>
2026-05-30 18:29:41 +0300

    feat(hooks): add useActionCreators hook for simplified Redux action binding
    Introduces a new hook that binds Redux action creators with dispatch using
    bindActionCreators, providing a cleaner API for dispatching actions in
    components. Updates sort-list component to use the new hook and exports
    mainProcessActions for convenient action access.

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,6 +1,18 @@
+import { ActionCreatorsMapObject, bindActionCreators } from '@reduxjs/toolkit';
+import { useMemo } from 'react';
 import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
 
 import { AppDispatch, State } from '../types/store';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
+
+export const useActionCreators = <Actions extends ActionCreatorsMapObject>(
+	actions: Actions
+): Actions => {
+	const dispatch = useAppDispatch();
+	return useMemo(
+		() => bindActionCreators(actions, dispatch),
+		[actions, dispatch]
+	);
+};
