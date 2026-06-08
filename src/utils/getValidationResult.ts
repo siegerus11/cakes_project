48a1697276a74b0057d1607c52039d74bd68d3fa
Siegerus <ivanoffsiegerus@gmail.com>
2026-06-08 19:20:34 +0300

    style(utils): format validation utility functions

--- src/utils/getValidationResult.ts
+++ src/utils/getValidationResult.ts
@@ -2,23 +2,20 @@ const nameValidate = (value: string): boolean | string => {
 	if (!value || !value.length) return 'Введите имя';
 	if (!/^[a-zA-Zа-яА-Я\s-]+$/.test(value))
 		return 'Имя должно содержать только буквы';
+
 	return true;
 };
 
 const phoneValidate = (value: string): boolean | string => {
-	if (!value || !value.length) {
-		return 'Введите номер телефона';
-	}
+	if (!value || !value.length) return 'Введите номер телефона';
 
 	const digits = value.replace(/\D/g, '');
 
 	if (
 		digits.length === 11 &&
 		(digits.startsWith('7') || digits.startsWith('8'))
-	) {
+	)
 		return true;
-	}
-
 	return 'Номер телефона должен быть в формате +7 999 999 99 99';
 };
 
@@ -28,15 +25,13 @@ const addressValidate = (value: string): boolean | string => {
 };
 
 const commentValidate = (value: string): boolean | string => {
-	if (value && value.length > 300) {
+	if (value && value.length > 300)
 		return 'Комментарий не должен превышать 300 символов';
-	}
 	return true;
 };
 
 const promoValidate = (value: string) => {
 	if (!value || value.length < 6) return 'Введите минимум 6 символов';
-
 	return true;
 };
 
